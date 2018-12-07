e8ffa8b53e7873056ae1fbc747b48d06691d7844
*p@KGCA114 <pointerphile@gmail.com>
2018-12-07 21:47:04 +0900

    progress

--- libppnetwork/PPTCPIOCPServer.cpp
+++ libppnetwork/PPTCPIOCPServer.cpp
@@ -2,3 +2,15 @@
 
 PP::PPTCPIOCPServer::PPTCPIOCPServer() {}
 PP::PPTCPIOCPServer::~PPTCPIOCPServer() {}
+
+int PP::PPTCPIOCPServer::Init() {
+	return 0;
+}
+
+int PP::PPTCPIOCPServer::Run() {
+	return 0;
+}
+
+int PP::PPTCPIOCPServer::Release() {
+	return 0;
+}
