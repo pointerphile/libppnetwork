1646b24fdf5940bff30b2a66f05661dbe0d301db
*p@KGCA114 <pointerphile@gmail.com>
2019-01-09 17:21:51 +0900

    libppnetwork [Version 0.1.0005]

--- libppnetwork/PPNetworkObject.h
+++ libppnetwork/PPNetworkObject.h
@@ -10,7 +10,7 @@ namespace PP {
 #ifdef _DEBUG
 		wchar_t m_wcharVersion[44] = L"libppnetwork [Version 0.1.0005] Debug Mode\n";
 #else
-		wchar_t m_wcharVersion[33] = L"libppnetwork [Version 0.1.0004]\n";
+		wchar_t m_wcharVersion[33] = L"libppnetwork [Version 0.1.0005]\n";
 #endif // DEBUG
 		
 	public:
